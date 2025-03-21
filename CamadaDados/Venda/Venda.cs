245ede118088ab869ce47281683789c27b9608cc
RicardoABrandao <ricardobrandao90@hotmail.com>
2025-03-21 20:13:32 +0000

    sql microsoft

--- CamadaDados/Venda/Venda.cs
+++ CamadaDados/Venda/Venda.cs
@@ -4,13 +4,61 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
 
 namespace CamadaDados.Venda
 {
     public class Venda
     {
+
+
+
         #region Methods
 
+        public static SqlConnection OpenDatabase(string connectionString)
+        {
+            SqlConnection dbConnection = new SqlConnection(connectionString);
+            dbConnection.Open();
+            return dbConnection;
+        }
+
+        public static DataTable RetrieveList(string storedProcedureName, string connectionString, out string errorMessage)
+        {
+            DataTable dataTable = null;
+            errorMessage = string.Empty;
+            try
+            {
+                SqlConnection dbConnection = GlobalDB.OpenDatabase(connectionString);
+
+                SqlCommand dbCommand = new SqlCommand(storedProcedureName, dbConnection);
+                dbCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+                SqlDataReader dataReader = dbCommand.ExecuteReader(CommandBehavior.SingleResult);
+
+                dataTable = new DataTable();
+                dataTable.Load(dataReader);
+
+                dbCommand.Dispose();
+                dbConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+            return dataTable;
+        }
+
+        // falta adicionar o parametro de data inicial e final
+        public static DataTable Listar(out string erro)
+        {
+            DataTable dataTable = GlobalDB.RetrieveList("ListarVendas", Properties.Settings.Default.ConnectionString, out erro);
+
+            return dataTable;
+        }
         #endregion
+
+
+
     }
 }
