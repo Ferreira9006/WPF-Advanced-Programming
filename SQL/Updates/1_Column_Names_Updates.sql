3f88431d9cfe272beda7c33e2094ab4c37a3d32b
Gabriel Ferreira <ferreirawow@gmail.com>
2025-03-21 20:02:08 +0000

    sql updates

---
+++ SQL/Updates/1_Column_Names_Updates.sql
@@ -0,0 +1,3 @@
+EXEC sp_rename 'Venda.Data', 'DataVenda', 'COLUMN';
+EXEC sp_rename 'Marca.Descricao', 'DescricaoMarca', 'COLUMN';
+EXEC sp_rename 'Vendedor.Nome', 'NomeVendedor', 'COLUMN';
---
+++ SQL/Updates/2_Stored_Procedure.sql
@@ -0,0 +1,19 @@
+GO
+CREATE PROCEDURE ListarVendas 
+@dataInicio datetime, @dataFim datetime
+AS
+
+BEGIN
+SELECT 
+    v.VendaID, 
+    v.DataVenda, 
+    v.Preco, 
+    ve.NomeVendedor,
+    m.DescricaoMarca
+FROM Venda v
+JOIN Marca m ON v.MarcaID = m.MarcaID
+JOIN Vendedor ve ON v.VendedorID = ve.VendedorID
+WHERE v.DataVenda BETWEEN @dataInicio AND @dataFim
+
+END
+GO
